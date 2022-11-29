bf47db2a0c95edd78cc49315590d584f0007a1ff
Karen Harutyunyan <Karen Harutyunyan@PC103>
2022-11-29 15:04:22 +0400

    Добавьте файлы проекта.

---
+++ ArrayHomework/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayHomework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            // drakan tveri mijin tvabanakan
+            Console.WriteLine("211 - Drakan tveri mijin tramabanakan");
+            int[] num = new int[] { -7, 5, 16, -3, 2 };
+
+            int numAccount = 0; // drakan tarreri gumar
+            int numBccount = 0; // drakan tarreri qanak
+            double sumQar = 0; // arjeqneri qarakusineri gumar
+
+            for (int i = 0; i < num.Length; i++)
+            {
+                if (num[i] > 0)
+                {
+                    numAccount += num[i];
+                    numBccount++;
+                    sumQar += num[i] * num[i];
+                }
+
+            }
+            Console.WriteLine("drakan tarreri gumar: " + numAccount);
+            Console.WriteLine("drakan tarreri qanak: " + numBccount);
+            Console.WriteLine("mijin tvabanakan: " + (double)numAccount / numBccount); // mijin tvabanakan
+
+            //212 
+            Console.WriteLine("\n212 - Drakan tareri mijin qarakusajiny");
+            double numMidQar = (double)sumQar / numBccount;
+            Console.WriteLine("drakan tarreri mijin qarakusajin: " + Math.Sqrt(numMidQar));// drakan tarreri mijin qarakusajin
+
+
+            Console.WriteLine();
+            //213
+            Console.WriteLine("\n213 - bacasakan tarreri mijin qarakusajin");
+
+            int[] myArray = new int[] { 48, -12, 74, -23, 56, -41 };
+
+            int sumBarray = 0; //bacasakan tveri gumar
+            int countBarray = 0; //bacasakan tveri qanak
+            double sumQarray = 0;
+
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                if (myArray[i] < 0)
+                {
+                    sumBarray += myArray[i];
+                    countBarray++;
+                    sumQarray += myArray[i] * myArray[i];
+                }
+            }
+            Console.WriteLine("Bacasakan tarreri gumary: " + sumBarray);
+            Console.WriteLine("Bacasakan tarreri qanaky: " + countBarray);
+            Console.WriteLine(sumQarray);
+            Console.WriteLine("Bacasakan tarreri mijin qarakusayiny: " + Math.Sqrt(sumQarray / countBarray));
+
+            //214
+            Console.WriteLine("\n214 - Bacasakan tarreri mijin tvabanakan");
+            double sumMathMid = (double)sumBarray / countBarray;
+            Console.WriteLine("bacasakan tarreri mijin tvabanakan: " + sumMathMid);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ArrayHomework1/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayHomework1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array1 = new int[] { 5, 9, -69, -89, 45, 35, 73 };
+
+            //215
+            Console.WriteLine("215 - Zuyg index unecux tarreri guary");
+
+
+            int sumPlus = 0;
+
+            for (int i = 1; i < array1.Length; i++) // i = 1, vor 0-n zuyg chhashvi
+            {
+                if (i % 2 == 0)
+                {
+                    sumPlus += array1[i]++;
+                }
+            }
+            Console.WriteLine("Zuyg index unecux tarreri guary: " + sumPlus);
+
+            //216
+            Console.WriteLine("216 - Zuyg index unecux tarreri artadryaly");
+
+            int sumPro = 1;
+            for (int i = 1; i < array1.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    sumPro *= array1[i];
+                }
+            }
+            Console.WriteLine("Zuyg index unecux tarreri artadryaly: " + sumPro);
+
+            //217
+            Console.WriteLine("Kent index unecox tarreri qarakusineri artadryal");
+            long sumBaPro = 1;
+            for (int i = 1; i < array1.Length; i++)
+            {
+                if (i % 2 > 0)
+                {
+                    sumBaPro *= array1[i] * array1[i];
+                }
+            }
+            Console.WriteLine("Kent index unecox tarreri qarakusineri artadryaly: " + sumBaPro);
+
+            //218
+            //Console.WriteLine("218 - Kent index unecox tarreri bacardzak arjeqneri gumar");
+            //int bacasakan = 0;
+            //int drakany = 0;
+            //for (int i = 0; i < array1.Length; i++)
+            //{
+            //    if (i % 2 > 0 && array1[i] > 0)
+            //    {
+            //        drakany += array1[i];
+            //    }
+            //    if (i % 2 > 0 && array1[i] < 0)
+            //    {
+            //        bacasakan += array1[i];
+            //    }
+            //}
+            //Console.WriteLine("Kent index unecox tarreri bacardzak arjeqneri gumar: " + drakany + (-1 * bacasakan));
+
+            Console.ReadKey();
+        }
+    }
+}
